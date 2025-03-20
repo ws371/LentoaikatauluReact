e734d4f6114114ddf7bf03536d9eda7e30e96a02
ws371 <waltteri.soininen@protonmail.com>
2025-03-20 19:56:20 +0200

    minor change

--- src/components/StatusTextComponent.js
+++ src/components/StatusTextComponent.js
@@ -5,7 +5,7 @@ function StatusTextComponent({selectedflight}) {
     return (
         <div className="p">
             {selectedflight ? (
-                <p>{description} {selectedflight.Lennonnumero} → {selectedflight.Kohde}</p>
+                <p>{description} {selectedflight.Lennonnumero} -> {selectedflight.Kohde}</p>
             ) : (
                 <p>Valitse lento nähdäksesi tiedot</p>
             )}
